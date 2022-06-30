f1a6158afd4da92fa3c1a5ec8e6cad93a2351596
ViniciusFelce <97105641+ViniciusFelce@users.noreply.github.com>
2022-06-30 00:55:11 -0300

    addTarefa(Error)

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -11,8 +11,47 @@ if (localStorage.getItem('Token') == null || localStorage.getItem('Token') == ''
 //Sessão Finalizada
 deslogar.addEventListener('click', () => {
     localStorage.removeItem('jwt');
-    alert('Sessão Finalizada!')
+    alert('Finalizar Sessão?')
     setTimeout(() => {
         window.localStorage.href = 'index.html'
     },3000)
 })
+
+//addTarefa( sem resultado )
+form.addEventListener('submit', (e) => {
+    if(inputTarefa.nodeValue.length != 0 ){
+        function addTarefa() {
+            fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', {
+                method: 'POST',
+                headers:{
+                    Accept: "*/*, application/json, text/plain",
+                    "Content-Type": "application/json",
+                    "authorization": `${inputTarefa.getItem('jwt')}`
+                },
+                body: JSON.stringify({
+                    "description": `${inputTarefa.velue}`,
+                    "completed": false
+                })
+            })
+            .then(res => ReadableStream.json())
+            .then(data => {
+                pendentes.innerHTML +=`
+                <li class="tarefa">
+                <div class="not-done"></div>
+                <div class="descricao">
+                <p class="nome">${data.description}</p>
+                <p class="timestamp">${data.createdAt}</p>
+                </div>
+                </li>
+                `
+                console.log('test1')
+            })
+        }
+    } else if (inputTarefa !== '' && inputTarefa != null){
+        alert('Tarefa Invalida!')
+        console.log('test2')
+    }
+
+})
+addTarefa();
+e.preventDefault();
