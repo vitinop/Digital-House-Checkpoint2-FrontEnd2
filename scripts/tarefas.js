c7848d62227d587a903ea9de9b7dbbd6d3490388
ViniciusFelce <97105641+ViniciusFelce@users.noreply.github.com>
2022-06-29 22:46:11 -0300

    BT Finalizar Sessão

--- scripts/tarefas.js
+++ scripts/tarefas.js
@@ -1,16 +1,18 @@
-const deslogar = document.getElementById('#closeApp');
+const deslogar = document.getElementById('closeApp');
 const form = document.getElementById('nova-tarefa');
 const inputTarefa = document.getElementById('novaTarefa');
 
-if (localStorage.getItem('jwt') == null || localStorage.getItem('jwt') == ''){
-    alert('Entre com a sua conta de usuário!, caso não tenha cadastre-se!');
+if (localStorage.getItem('Token') == null || localStorage.getItem('Token') == ''){
+    alert('Entre com a sua conta de usuário, caso não tenha cadastre-se!');
     window.location.href = 'index.html'
     // incompatível com o FireFox!!!
 }
 
-deslogar.addEventListener('click'), () => {
+//Sessão Finalizada
+deslogar.addEventListener('click', () => {
     localStorage.removeItem('jwt');
+    alert('Sessão Finalizada!')
     setTimeout(() => {
         window.localStorage.href = 'index.html'
-    },1000)
-}
+    },3000)
+})
